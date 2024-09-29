30ace3b7956bb09c05c55a7093437069271edbb0
raniskalol <mikerobello9444@gmail.com>
2024-09-29 17:58:57 +0300

    first commit

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let name = prompt ("Ваше Имя");
+let Surname = prompt ("Ваша фамилия");
+let Pastname = prompt ("Ваше отчество");
+ 
+let istrue = confirm (`${name} ${Surname} ${Pastname}`);
+
+alert (istrue);
